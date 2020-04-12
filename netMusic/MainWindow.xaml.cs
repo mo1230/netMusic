05f966413d39a0ac81551d33463414a79d70e0b0
mo1230 <1144898671@qq.com>
2020-04-12 12:37:41 +0800

    完成左侧侧边栏点击后切换页面

--- netMusic/MainWindow.xaml.cs
+++ netMusic/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using netMusic.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace netMusic
 {
     /// <summary>
@@ -19,9 +21,21 @@ namespace netMusic
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PageManage pageManage = new PageManage();
+        private MainPage mainPage = new MainPage();
+        public MainPage MainPage { 
+            get {
+                return mainPage;
+            } 
+            set {
+                mainPage = value;
+            } 
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
@@ -49,6 +63,42 @@ namespace netMusic
         private void AppHeader_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void BtnFind_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.FindMusicPage;
+            
+        }
+
+        private void BtnFM_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.FMPage;
+        }
+
+        private void BtnLOOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.LOOKPage;
+        }
+
+        private void BtnVideo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.VideoPage;
+        }
+
+        private void BtnFriend_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.FriendPage;
+        }
+
+        private void BtnLocalMusic_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.LocalMusicPage;
+        }
+
+        private void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Content = pageManage.DownloadPage;
         }
     }
 }
---
+++ netMusic/PageManage.cs
@@ -0,0 +1,57 @@
+﻿using netMusic.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netMusic
+{
+    class PageManage 
+    {
+        private DownloadPage downloadPage;
+        private FindMusicPage findMusicPage;
+        private FMPage fMPage;
+        private FriendPage friendPage;
+        private LocalMusicPage localMusicPage;
+        private LOOKPage lOOKPage;
+        private MainPage mainPage;
+        private VideoPage videoPage;
+
+        public PageManage()
+        {
+            this.downloadPage = new DownloadPage();
+            this.findMusicPage = new FindMusicPage();
+            this.fMPage = new FMPage();
+            this.friendPage = new FriendPage();
+            this.localMusicPage = new LocalMusicPage();
+            this.lOOKPage = new LOOKPage();
+            this.mainPage = new MainPage();
+            this.videoPage = new VideoPage();
+        }
+
+        public DownloadPage DownloadPage
+        {
+            get
+            {
+                return downloadPage;
+            }
+            set
+            {
+                downloadPage = value;
+            //    OnPropertyChanged(new PropertyChangedEventArgs("DownloadPage"));
+            }
+        }
+
+        public FindMusicPage FindMusicPage { get => findMusicPage; set => findMusicPage = value; }
+        public FMPage FMPage { get => fMPage; set => fMPage = value; }
+        public FriendPage FriendPage { get => friendPage; set => friendPage = value; }
+        public LocalMusicPage LocalMusicPage { get => localMusicPage; set => localMusicPage = value; }
+        public LOOKPage LOOKPage { get => lOOKPage; set => lOOKPage = value; }
+        public MainPage MainPage { get => mainPage; set => mainPage = value; }
+        public VideoPage VideoPage { get => videoPage; set => videoPage = value; }
+
+     
+    }
+}
